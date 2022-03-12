d299847ff347f547b5ebe284f148012a4bde32b2
Guillaume Gricourt <guipagui@gmail.com>
2022-03-12 13:45:08 +0100

    feat(utils): add check_bam_index()

--- src/hmnfusion/utils.py
+++ src/hmnfusion/utils.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import pysam
 import re
 import shutil
 import subprocess
@@ -98,6 +99,33 @@ def cmdline(args: List[str], logger: logging.Logger = logging.getLogger()) -> in
     if ret.stderr is not None:
         logging.warning(ret.stderr)
     return ret.returncode
+
+
+def check_bam_index(path: str) -> bool:
+    """Build index file for a bam file if not found.
+
+    Parameters
+    ----------
+    path: str
+        Path of the bam file
+
+    Return
+    ------
+    bool
+        True if index is present or is written, False otherwise.
+    """
+    alignment = pysam.AlignmentFile(path)
+    try:
+        alignment.check_index()
+    except ValueError as e:
+        pysam.index(path)
+    finally:
+        alignment.close()
+    try:
+        alignment = pysam.AlignmentFile(path, require_index=True)
+        return True
+    except Exception:
+        return False
 
 
 def find_executable(executable: str, msg: str = "") -> None:
